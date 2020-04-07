a776dc18d228923b3da6f8f0f1d06cbede396aa2
danilgugu <danil.gugu52@gmail.com>
2020-04-07 13:49:05 +0300

    less code duplication

--- src/main/java/helper/StatisticsFormatter.java
+++ src/main/java/helper/StatisticsFormatter.java
@@ -4,26 +4,26 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StatisticsFormatter {
 
     public static List<String> formatByHours(Map<LocalDateTime, Long> errorCounterMap) {
-        return errorCounterMap.entrySet().stream()
-                .map(entry -> {
-                    LocalDateTime localDateTime = entry.getKey();
-                    LocalTime localTime = localDateTime.toLocalTime();
-                    return localDateTime.toLocalDate() + ", " + TimeRangeHelper.getTimeRange(localTime) +
-                            " Количество ошибок: " + entry.getValue();
-                }).collect(Collectors.toList());
+        return format(errorCounterMap, TimeRangeHelper::getTimeRange);
     }
 
     public static List<String> formatByMinutes(Map<LocalDateTime, Long> errorCounterMap) {
+        return format(errorCounterMap, LocalTime::toString);
+    }
+
+    private static List<String> format(Map<LocalDateTime, Long> errorCounterMap,
+                                       Function<LocalTime, String> timeRangeFormatter) {
         return errorCounterMap.entrySet().stream()
                 .map(entry -> {
                     LocalDateTime localDateTime = entry.getKey();
                     LocalTime localTime = localDateTime.toLocalTime();
-                    return localDateTime.toLocalDate() + ", " + localTime +
+                    return localDateTime.toLocalDate() + ", " + timeRangeFormatter.apply(localTime) +
                             " Количество ошибок: " + entry.getValue();
                 }).collect(Collectors.toList());
     }
